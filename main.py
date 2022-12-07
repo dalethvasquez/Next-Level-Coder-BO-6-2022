01a6d8585b95341376de72ccad594b7595a4de4d
daleth055 <115034511+dalethvasquez@users.noreply.github.com>
2022-12-07 19:57:29 -0400

    class 3: obstacles

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from dino_runner.components.game import Game
+import pygame
 
 if __name__ == "__main__":
     game = Game()
     game.run()
     print("hello there...")
+    pygame.mixer.music.load("la-atmosfera_4.mp3")
+    pygame.mixer.play(5)
